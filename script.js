9f91a45de337b6f2afce51eec1f4092d8f2051cb
seli kwadzovia <selikemkwadz@gmail.com>
2024-09-22 10:09:06 +0000

    render multiple in center

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ const boxesPerRow = 4;
 
 let boxSelector = document.querySelector("#box-container");
 
-let boxInstance = document.createElement("div");
-boxInstance.style.height = "2rem";
-boxInstance.style.width = "2rem";
-boxInstance.style.backgroundColor = "blue";
-boxInstance.style.border = "3px,solid,black";
-boxInstance.classList.add("box")
-boxSelector.appendChild(boxInstance);
+
+
+for (let i = 0; i < boxesPerRow; i++) {
+
+    let boxInstance = document.createElement("div");
+    boxInstance.style.height = "2rem";
+    boxInstance.style.width = "2rem";
+    boxInstance.style.backgroundColor = "blue";
+    boxInstance.style.border = "3px,solid,black";
+    boxInstance.classList.add("box")
+
+    boxSelector.appendChild(boxInstance);
+
+}
