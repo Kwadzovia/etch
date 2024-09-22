1f3b2e92e583a55328b88accf71b743dcb86875c
seli kwadzovia <selikemkwadz@gmail.com>
2024-09-22 10:33:28 +0000

    initialize grid

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const boxesPerRow = 4;
+const boxesPerRow = 8;
+const boxSideLength = 2;
+const boxSideLengthUnit = "rem"
 
 let boxSelector = document.querySelector("#box-container");
 
+//Set max column width before wrapping
+boxSelector.style.width = `${boxesPerRow * boxSideLength}` + boxSideLengthUnit;
 
-
-for (let i = 0; i < boxesPerRow; i++) {
+//Render boxes
+for (let i = 0; i < boxesPerRow ** 2; i++) {
 
     let boxInstance = document.createElement("div");
-    boxInstance.style.height = "2rem";
-    boxInstance.style.width = "2rem";
-    boxInstance.style.backgroundColor = "blue";
-    boxInstance.style.border = "3px,solid,black";
+    boxInstance.style.width = boxSideLength + "rem";
+    boxInstance.style.height = boxInstance.style.width;
+    boxInstance.style.backgroundColor = "beige";
+    boxInstance.style.border = "2px,solid,black";
     boxInstance.classList.add("box")
 
     boxSelector.appendChild(boxInstance);
-
 }
