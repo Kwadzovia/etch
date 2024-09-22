85f2766af25bfe4711ccebaa21edf999ce93eb1c
seli kwadzovia <selikemkwadz@gmail.com>
2024-09-22 13:12:38 +0000

    add hovering rgb trail

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const boxesPerRow = 8;
+const boxesPerRow = 16;
 const boxSideLength = 2;
 const boxSideLengthUnit = "rem"
 
 let boxSelector = document.querySelector("#box-container");
+
 
 //Set max column width before wrapping
 boxSelector.style.width = `${boxesPerRow * boxSideLength}` + boxSideLengthUnit;
@@ -13,9 +14,18 @@ for (let i = 0; i < boxesPerRow ** 2; i++) {
     let boxInstance = document.createElement("div");
     boxInstance.style.width = boxSideLength + "rem";
     boxInstance.style.height = boxInstance.style.width;
-    boxInstance.style.backgroundColor = "beige";
     boxInstance.style.border = "2px,solid,black";
+    boxInstance.style.backgroundColor = "beige";
     boxInstance.classList.add("box")
-
+    boxInstance.classList.add("default-color")
     boxSelector.appendChild(boxInstance);
 }
+
+boxSelector.addEventListener("mouseover", (e) => {
+    let r = Math.floor(Math.random() * 255);
+    let g = Math.floor(Math.random() * 255);
+    let b = Math.floor(Math.random() * 255);
+    // console.log(`rgb(${r},${g},${g})`)
+    e.target.style.backgroundColor = `rgb(${r},${g},${g})`;
+})
+
